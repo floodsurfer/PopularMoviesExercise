86a2d942104fe3c117a704a03e7147f1743f48ff
floodsurfer <mrlip@hotmail.com>
2015-07-19 00:21:32 -0400

    Refresh button sends HTTP request via AsyncTask and receives JSON string response

--- app/src/main/java/com/example/jlipatap/popularmoviesexercise/MainActivityFragment.java
+++ app/src/main/java/com/example/jlipatap/popularmoviesexercise/MainActivityFragment.java
@@ -1,8 +1,14 @@
 package com.example.jlipatap.popularmoviesexercise;
 
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -12,6 +18,12 @@ import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -22,17 +34,24 @@ import java.util.List;
  */
 public class MainActivityFragment extends Fragment {
 
+    public String LOG_TAG = "MainActivityFragment";
+
     public MainActivityFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-
-
 
        // ArrayAdapter<Integer> arrayAdapter = new ImageAdapter<Integer>(getActivity(),R.layout.fragment_main, R.id.grid_view, sampleImagesArrayList);
 
@@ -44,7 +63,7 @@ public class MainActivityFragment extends Fragment {
         gridview.setOnItemClickListener(new OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v,
                                     int position, long id) {
-                Toast.makeText(getActivity(), "" + position,
+                Toast.makeText(getActivity(), APIKey.getKey()+" " + position,
                         Toast.LENGTH_SHORT).show();
             }
         });
@@ -61,5 +80,109 @@ public class MainActivityFragment extends Fragment {
         });*/
 
         return rootView;
+    }
+
+    public void onCreateOptionsMenu (Menu menu, MenuInflater inflater){ //Not sure where "menu" is coming
+        // from.  Is this just something the framework passes when calling this method?
+
+        inflater.inflate(R.menu.movieslayout, menu);
+        //return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if(id == R.id.action_refresh){
+
+            String movieSortSetting = TmdbApiHandler.TMDB_SORT_BY_POPULARITY;
+
+            FetchMoviesTask fetchMoviesTask = new FetchMoviesTask();
+            fetchMoviesTask.execute(movieSortSetting);
+
+            //AsyncTask asyncTask = new FetchMoviesTask().execute(); // Why isn't this type FetchMoviesTask
+                    // instead of AsyncTask?  And why don't i need to pass Params to it as per api doc
+
+
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    public class FetchMoviesTask extends AsyncTask<String, Void, Void>{
+
+
+        protected Void doInBackground(String... params) { //I don't understand this syntax.  Why "Void"
+                // instead of "void"? How does "String... params" work?
+
+
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String movieJsonStr = null;
+
+
+            try {
+                // Construct the URL for the API query TODO: Update this to URI
+                URL url = new URL("http://api.themoviedb.org/3/discover/movie?api_key="+ APIKey.getKey() +"&" +params[0]);
+
+
+
+                // Create the request to TMDB and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                Log.d(LOG_TAG, "urlConnection.connect() called" + url.toString());
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    movieJsonStr = null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    movieJsonStr = null;
+                }
+                movieJsonStr = buffer.toString();
+                Log.v(LOG_TAG, movieJsonStr); //Comment this out when done.  This is going to be huge.
+
+            } catch (IOException e) {
+                Log.e("PlaceholderFragment", "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attempting
+                // to parse it.
+                movieJsonStr = null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("PlaceholderFragment", "Error closing stream", e);
+                    }
+                }
+            }
+        return null;
+        }
+
     }
 }
--- app/src/main/java/com/example/jlipatap/popularmoviesexercise/TmdbApiHandler.java
+++ app/src/main/java/com/example/jlipatap/popularmoviesexercise/TmdbApiHandler.java
@@ -4,4 +4,11 @@ package com.example.jlipatap.popularmoviesexercise;
  * Created by jlipatap on 7/18/15.
  */
 public class TmdbApiHandler {
+
+    public static String TMDB_BASEURL = "http://api.themoviedb.org/3/discover/movie";
+    public static String TMDB_API_KEY = APIKey.getKey();    //This is stored in a separate class because it cannot be uploaded to GIT
+    public static String TMDB_SORT_BY_POPULARITY = "sort_by=popularity.desc";
+    public static String TMDB_SORT_BY_RATING = "sort_by=vote_average.desc";
+
+
 }
